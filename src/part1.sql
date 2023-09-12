73e2ae6321e2be079961a30f37d02811157a94f4
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-12 14:55:44 +0300

    creation of all tables

--- src/part1.sql
+++ src/part1.sql
@@ -1,9 +1,79 @@
 create table if not exists P2P
 (
 	id bigint primary key,
-	Check bigint primary key,
-	Checkingpeer varchar,
-	State varchar,
+	Checks integer,
+	CheckingPeer varchar,
+	state varchar,
 	created  timestamp with time zone not null
-    default (current_timestamp at time zone 'UTC'), 
-)
+    default (current_timestamp at time zone 'UTC')
+);
+
+create table if not exists TransferredPoints
+(
+	id bigint primary key,
+	CheckingPeer varchar,
+	CheckedPeer varchar,
+	PointsAmount integer
+);
+
+create table if not exists Friends
+(
+	id bigint primary key,
+	Peer1 varchar,
+	Peer2 varchar
+);
+
+create table if not exists Recommendations
+(
+	id bigint primary key,
+	Peer varchar,
+	RecommendatedPeer varchar
+);
+
+create table if not exists TimeTracking
+(
+	id bigint primary key,
+	Peer varchar,
+	Date date not null,
+	created timestamp with time zone not null default current_timestamp,
+	State integer
+);
+
+create table if not exists Checks
+(
+	id bigint primary key,
+	Peer varchar,
+	Task varchar,
+	Date date not null
+);
+
+create table if not exists Peers
+(
+	NickName varchar primary key,
+	Birthday date not null
+);
+
+create table if not exists Verter
+(
+	id bigint primary key,
+	Checks bigint,
+	State varchar,
+	created timestamp with time zone not null default current_timestamp
+);
+
+create table if not exists XP
+(
+	id bigint primary key,
+	Checks bigint,
+	XPAmount bigint
+);
+
+create table if not exists Tasks
+(
+	Title varchar primary key,
+	ParentTask varchar,
+	MaxXP bigint
+);
+
+
+
