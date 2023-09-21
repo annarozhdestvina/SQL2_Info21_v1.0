f96e50c6064563f2045e42715d34eff3f076c7b9
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-21 17:31:53 +0300

    limit 1

--- src/part2.sql
+++ src/part2.sql
@@ -1,14 +1,3 @@
--- Write a procedure for adding P2P check
-
-CREATE or replace PROCEDURE test3(CheckedPeer varchar, CheckingPeer varchar, task_name varchar, p2p_check_status check_status, time_check time)
-language sql
-AS $$
-select * from checks;
-$$;
-
-
-
-////////////////
 CREATE or replace PROCEDURE test3(CheckedPeer varchar, CheckingPeer varchar, task_name varchar, p2p_check_status check_status, time_check time)
 language plpgsql
 
@@ -16,67 +5,26 @@ AS $$
 declare 
 	check_id integer;
 begin 
-	if (p2p_check_status == 'Start') then
-		select max(id)+1 from checks into check_id;
-		insert into checks(id, peer, task, date)
-		values (check_id, CheckedPeer, task_name, current_date);
-		
-		insert into p2p(id, checks, checkingpeer, check_state, created)
-		values ((select max(id)+1 from p2p), check_id, CheckingPeer, task_name, current_date);
-
-	end if;
-
-end;
-$$;
-
-
-
-
-
-
-/////////////////////////
-
-CREATE or replace PROCEDURE test3(CheckedPeer varchar, CheckingPeer varchar, task_name varchar, p2p_check_status check_status, time_check time)
-language plpgsql
-
-AS $$
-declare 
-	check_id integer;
-begin 
-	if (p2p_check_status == 'Start') then
-		select max(id)+1 from checks into check_id;
-		insert into checks(id, peer, task, date)
-		values (check_id, CheckedPeer, task_name, current_date);
-		
-	else
-		select checks from p2p where checkingpeer == CheckingPeer and state == 'Start' into check_id;
-
+	if not (select exists (select p2p.checkingpeer from p2p where p2p.checkingpeer = CheckingPeer))
+	then raise 'not found checkingpeer';
 	end if;
 	
-	insert into p2p(id, checks, checkingpeer, check_state, created)
-	values ((select max(id)+1 from p2p), check_id, CheckingPeer, task_name, current_date);
-		
-
-end;
-$$;
-
-/// limit 1
-
-CREATE or replace PROCEDURE test3(CheckedPeer varchar, CheckingPeer varchar, task_name varchar, p2p_check_status check_status, time_check time)
-language plpgsql
-
-AS $$
-declare 
-	check_id integer;
-begin 
+	if not (select exists (select p2p.checkedpeer from p2p where p2p.checkedpeer = CheckedPeer))
+	then raise 'not found checkedpeer';
+	
+	end if;
 	if (p2p_check_status == 'Start') then
 		select max(id)+1 from checks into check_id;
 		insert into checks(id, peer, task, date)
 		values (check_id, CheckedPeer, task_name, current_date);
 		
 	else
-		select checks from p2p where checkingpeer == CheckingPeer and state == 'Start' 
+		select checks from p2p where checkingpeer = CheckingPeer and state = 'Start' 
 		order by id desc limit 1 into check_id;
+		
+		if check_is is null then
+			raise 'no start status for that checking peer';
+		end if;
 
 	end if;
 	
