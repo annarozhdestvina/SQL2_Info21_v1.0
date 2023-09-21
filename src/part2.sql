83260423f651bcc75c2342068930b10fe7da3da0
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-21 18:45:39 +0300

    develop/ create pricedure

--- src/part2.sql
+++ src/part2.sql
@@ -1,25 +1,27 @@
-CREATE or replace PROCEDURE test3(CheckedPeer varchar, CheckingPeer varchar, task_name varchar, p2p_check_status check_status, time_check time)
+-- Write a procedure for adding P2P check
+
+CREATE or replace PROCEDURE test23(PCheckedPeer varchar, PCheckingPeer varchar, task_name varchar, p2p_check_status check_status, time_check time)
 language plpgsql
 
 AS $$
 declare 
 	check_id integer;
 begin 
-	if not (select exists (select p2p.checkingpeer from p2p where p2p.checkingpeer = CheckingPeer))
-	then raise 'not found checkingpeer';
+	if not (select exists (select p2p.checkingpeer from p2p where p2p.checkingpeer = PCheckingPeer))
+	then raise 'not found checkingpeer azaza';
 	end if;
 	
-	if not (select exists (select p2p.checkedpeer from p2p where p2p.checkedpeer = CheckedPeer))
-	then raise 'not found checkedpeer';
+	if not (select exists (select p2p.checkedpeer from p2p where p2p.checkedpeer = PCheckedPeer))
+	then raise 'not found checkedpeer lol';
 	
 	end if;
 	if (p2p_check_status == 'Start') then
 		select max(id)+1 from checks into check_id;
 		insert into checks(id, peer, task, date)
-		values (check_id, CheckedPeer, task_name, current_date);
+		values (check_id, PCheckedPeer, task_name, current_date);
 		
 	else
-		select checks from p2p where checkingpeer = CheckingPeer and state = 'Start' 
+		select checks from p2p where p2p.checkingpeer = PCheckingPeer and state = 'Start' 
 		order by id desc limit 1 into check_id;
 		
 		if check_is is null then
@@ -29,36 +31,8 @@ begin
 	end if;
 	
 	insert into p2p(id, checks, checkingpeer, check_state, created)
-	values ((select max(id)+1 from p2p), check_id, CheckingPeer, task_name, current_date);
+	values ((select max(id)+1 from p2p), check_id, PCheckingPeer, task_name, current_date);
 		
 
 end;
 $$;
-
-
-
-
-//// 
-CREATE or replace PROCEDURE test4(n integer)
-language plpgsql
-
-AS $$
-declare
-begin 
-	insert into teeeest(id, checks, checkingpeer, state, created)
-		select * from p2p where id = n;
-		
-end;
-$$;
-
-select * from teeeest
-
-call test4(15);
-drop table teeeest
-create table teeeest (
-	id bigint primary key,
-	Checks integer,
-	CheckingPeer varchar,
-	state check_status,
-	created time  
-);
